0d0d08a6401d5cd9467ab98eff1d6d671f3e2b68
Amber Jones <amberlovescats14@gmail.com>
2019-11-15 08:21:26 -0600

    finished the arrays of products

---
+++ public/js/products/foundations.js
@@ -0,0 +1,67 @@
+const {uuid} = require('uuidv4')
+
+const foundations = [
+  {
+    id: uuid(),
+    color: `Foundation #100`,
+    description: "Fair-Neutral",
+    price: 35.00    
+  },{
+    id: uuid(),
+    color: `Foundation #105`,
+    description: "Fair-Warm",
+    price: 35.00    
+  }, {
+    id: uuid(),
+    color: `Foundation #110`,
+    description: "Fair-Pink",
+    price: 35.00    
+  }, {
+    id: uuid(),
+    color: `Foundation #130`,
+    description: "Fair-Olive",
+    price: 35.00   
+  }, {
+    id: uuid(),
+    color: `Foundation #270`,
+    description: "Medium-Neutral",
+    price: 35.00   
+  }, {
+    id: uuid(),
+    color: `Foundation #280`,
+    description: "Medium-Warm",
+    price: 35.00   
+  }, {
+    id: uuid(),
+    color: `Foundation #290`,
+    description: "Medium-Pink",
+    price: 35.00   
+  }, {
+    id: uuid(),
+    color: `Foundation #300`,
+    description: "Medium-Olive",
+    price: 35.00   
+  }, {
+    id: uuid(),
+    color: `Foundation #450`,
+    description: "Tan-Neutral",
+    price: 35.00   
+  }, {
+    id: uuid(),
+    color: `Foundation #460`,
+    description: "Tan-Warm",
+    price: 35.00   
+  }, {
+    id: uuid(),
+    color: `Foundation #470`,
+    description: "Tan-Pink",
+    price: 35.00   
+  }, {
+    id: uuid(),
+    color: `Foundation #500`,
+    description: "Tan-Olive",
+    price: 35.00   
+  }
+]
+
+export default foundations
