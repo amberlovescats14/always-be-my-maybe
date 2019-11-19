de7afbdc6681bc4632877308d5498ff4db252d0d
Amber Jones <amberlovescats14@gmail.com>
2019-11-19 14:31:03 -0600

    I made the cards and am able to handle add to cart:

---
+++ public/js/js-html/cart.js
@@ -0,0 +1,6 @@
+import $ from 'jquery'
+import cart from '../products/cart'
+
+$(document).ready(function () {
+    console.log('cart:', cart)
+})
--- public/js/js-html/foundations.js
+++ public/js/js-html/foundations.js
@@ -1,12 +1,60 @@
 import $ from 'jquery'
 import foundations from '../products/foundations'
+import cart from '../products/cart'
 
 $(document).ready(function(){
   let idArr = []
-  console.log("foundations: ", foundations)
-  let cardWrapper = $('#card-wrapper')
-  foundations.forEach((f,i)=> {
-  
-  })
+  let cardWrapper = $('#cards-wrapper')
+  const displayFoundations = () => {
+    foundations.forEach((f,i)=> {
+      idArr.push(f.id)
+      let html =
+          `<div class="col s12 m7 l2">
+            <div class="card"><br>
+            <div class="card-image card-img">
+                <img src="https://blog.andersonhopkins.com/wp-content/uploads/2014/08/051614_GenericFoundation_Smear_FINAL.jpg">
+            </div>
+            <div class="card-content center-align">
+                <p><h5>${f.color}</h5></p><br>
+                <p>${f.description}</p>
+                <p>${f.price}</p>
+            </div>
+            <div class="card-action center-align">
+                <a class="waves-effect waves-light btn"
+                id="foundation-${f.id}"><i class="material-icons small">
+                    shopping_basket</i></a>
 
+            </div>
+        </div>
+    </div>`
+      cardWrapper.append(html)
+    })
+    addToCart(idArr)
+  }
+
+  const addToCart = arr => {
+    arr.forEach(a => {
+      $(`#foundation-${a}`).click(function () {
+        console.log("clicked", a)
+        findItem(a)
+      })
+    })
+  }
+  
+  
+  const findItem = (num) => {
+    foundations.forEach((f,i)=> {
+      if(f.id === num){
+        console.log("found: ", f)
+        cart.push(f)
+        console.log("cart: ", cart)
+        return alert(`Foundation ${f.color} Added To Cart`)
+      }
+    })
+  }
+  
+  displayFoundations()
+  
 })
+
+
